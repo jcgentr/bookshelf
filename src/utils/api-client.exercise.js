7e66513359f2b7e5e199ee4bec77f562c416ff4a
Jared Gentry <jared.gentry@centauricorp.com>
2021-12-01 17:32:45 -0500

    finish authentication section

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,12 +1,24 @@
-const apiURL = process.env.REACT_APP_API_URL
+import * as auth from 'auth-provider'
 
+const apiURL = process.env.REACT_APP_API_URL
+// data: {name: 'Fluffy', type: 'cat'},
 function client(endpoint, customConfig = {}) {
+  const {token, data: body, ...customConfigRest} = customConfig
   const config = {
-    method: 'GET',
-    ...customConfig,
+    method: body ? 'POST' : 'GET',
+    headers: {
+      Authorization: token ? `Bearer ${token}` : undefined,
+    },
+    body,
+    ...customConfigRest,
   }
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
+    console.log(response)
+    if (response.status === 401) {
+      await auth.logout()
+      return window.location.assign(window.location)
+    }
     const data = await response.json()
     if (response.ok) {
       return data
